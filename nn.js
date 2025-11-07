bc48ff977f13fc29feaff5ef86cfb2e85db3e69e
Sk-1597 <sujithkumarcgnr@gmail.com>
2025-11-07 11:52:39 +0530

    app 5.5

--- nn.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-  const signOutBtn = document.getElementById("signOutBtn");
-  if (signOutBtn) {
-    signOutBtn.onclick = () => {
-      localStorage.removeItem("loggedUser");
-      window.location.href = "index.html"; // ✅ redirect to login page
-    };
-  }
-};
